edabdcde84b13fb4edf822ad127b47a27a49d7de
CraigMChen <476479827@qq.com>
2021-04-14 10:30:11 +0800

    modify lc162

--- lc162_Find_Peak_Element/ac1/1.go
+++ lc162_Find_Peak_Element/ac1/1.go
@@ -1,24 +1,14 @@
 package ac1
 
+// 二分
 func findPeakElement(nums []int) int {
-	l, r := 0, len(nums)
+	l, r := 0, len(nums) - 1 // 因为要比较nums[m]和nums[m + 1]，所以这里要-1，防止下标越界
 	for l < r {
-		if r == l + 1 {
-			return l
-		}
 		m := (r-l)>>1 + l
-		if m != r-1 {
-			if nums[m] < nums[m+1] {
-				l = m + 1
-			} else {
-				r = m
-			}
-		} else if m != l {
-			if nums[m] < nums[m-1] {
-				l = m + 1
-			} else {
-				r = m
-			}
+		if nums[m] < nums[m + 1] { // 若nums[m] < nums[m + 1]则峰值肯定在右侧
+			l = m + 1
+		} else { // 否则峰值肯定在左侧
+			r = m
 		}
 	}
 	return l
