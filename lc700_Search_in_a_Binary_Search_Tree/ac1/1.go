d8a42ac8a36e33246dee5e7af49dbf88552beb92
CraigMChen <476479827@qq.com>
2021-06-13 09:43:42 +0800

    add lc700

---
+++ lc700_Search_in_a_Binary_Search_Tree/ac1/1.go
@@ -0,0 +1,21 @@
+package ac1
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	for root != nil {
+		if root.Val == val {
+			return root
+		}
+		if root.Val > val {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+	return nil
+}
