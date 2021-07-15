0c195b61a3c5bc15fee960d3a9cf4b63f9e1c446
CraigMChen <476479827@qq.com>
2021-07-15 22:24:29 +0800

    update lc94 lc144

--- lc94_Binary_Tree_Inorder_Traversal/ac2/2.go
+++ lc94_Binary_Tree_Inorder_Traversal/ac2/2.go
@@ -6,20 +6,20 @@ type TreeNode struct {
 }
 
 // 迭代
-func preorderTraversal(root *TreeNode) []int {
+func inorderTraversal(root *TreeNode) []int {
 	var (
-		ans   []int
+		ans []int
 		stack []*TreeNode
 	)
 	node := root
 	for node != nil || len(stack) != 0 {
 		for node != nil {
-			ans = append(ans, node.Val)
 			stack = append(stack, node)
 			node = node.Left
 		}
-		node = stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
+		node = stack[len(stack) - 1]
+		stack = stack[:len(stack) - 1]
+		ans = append(ans, node.Val)
 		node = node.Right
 	}
 	return ans
