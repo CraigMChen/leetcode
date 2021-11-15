94e37794fc617253ecee7441bcf32a309f5243f8
CraigMChen <476479827@qq.com>
2021-11-15 22:08:05 +0800

    add lc53

---
+++ lc53_Maximum_Subarray/ac1/1.go
@@ -0,0 +1,15 @@
+package ac1
+
+// 动态规划
+func maxSubArray(nums []int) int {
+	res := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > res {
+			res = nums[i]
+		}
+	}
+	return res
+}
