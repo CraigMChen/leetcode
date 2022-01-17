08756cc4a52ac21b0b54be7c2a237651769855c0
CraigMChen <476479827@qq.com>
2022-01-17 21:34:03 +0800

    update lc118

--- lc118_Pascals_Triangle/ac1/1.go
+++ lc118_Pascals_Triangle/ac1/1.go
@@ -1,8 +1,28 @@
 package ac1
 
-func generate(numRows int) [][]int {
-	triangle := make([][]int, numRows)
-	for i := 0; i < numRows; i++ {
+//func generate(numRows int) [][]int {
+//	triangle := make([][]int, numRows)
+//	for i := 0; i < numRows; i++ {
+//		triangle[i] = make([]int, i+1)
+//		for j := 0; j <= i; j++ {
+//			if j == 0 || j == i {
+//				triangle[i][j] = 1
+//			} else {
+//				triangle[i][j] = triangle[i-1][j] + triangle[i-1][j-1]
+//			}
+//		}
+//	}
+//	return triangle
+//}
+
+
+// 预处理
+
+var triangle [][]int
+
+func init() {
+	triangle = make([][]int, 30)
+	for i := 0; i < 30; i++ {
 		triangle[i] = make([]int, i+1)
 		for j := 0; j <= i; j++ {
 			if j == 0 || j == i {
@@ -12,5 +32,8 @@ func generate(numRows int) [][]int {
 			}
 		}
 	}
-	return triangle
+}
+
+func generate(numRows int) [][]int {
+	return triangle[:numRows]
 }
