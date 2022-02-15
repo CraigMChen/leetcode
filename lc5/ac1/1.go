943032e9b7e28d2d792e6c2711a2baf727cab89a
CraigMChen <476479827@qq.com>
2022-02-15 09:25:23 +0800

    update lc5

--- lc5/ac1/1.go
+++ lc5/ac1/1.go
@@ -6,33 +6,61 @@ package ac1
 // 边际条件为
 // dp[i][i] = true
 // dp[i][i+1] = s[i] == s[i+1]
+//func longestPalindrome(s string) string {
+//	l := len(s)
+//	if l == 0 {
+//		return ""
+//	}
+//	dp := make([][]bool, l)
+//	for i := 0; i < l; i++ {
+//		dp[i] = make([]bool, l)
+//	}
+//	palindromeLen := 1
+//	start, end := 0, 0
+//	for k := 0; k < l; k++ {
+//		for i := 0; i+k < l; i++ {
+//			j := i + k
+//			if i == j {
+//				dp[i][j] = true
+//			} else if j == i+1 {
+//				dp[i][j] = s[i] == s[j]
+//			} else {
+//				dp[i][j] = dp[i+1][j-1] && s[i] == s[j]
+//			}
+//			if dp[i][j] && j-i+1 > palindromeLen {
+//				palindromeLen = j - i + 1
+//				start = i
+//				end = j
+//			}
+//		}
+//	}
+//	return s[start : end+1]
+//}
+
+// 2022.02.14
 func longestPalindrome(s string) string {
-	l := len(s)
-	if l == 0 {
-		return ""
+	dp := make([][]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]bool, len(s))
 	}
-	dp := make([][]bool, l)
-	for i := 0; i < l; i++ {
-		dp[i] = make([]bool, l)
-	}
-	palindromeLen := 1
+
+	l := 1
 	start, end := 0, 0
-	for k := 0; k < l; k++ {
-		for i := 0; i+k < l; i++ {
-			j := i + k
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := i; j < len(s); j++ {
 			if i == j {
 				dp[i][j] = true
-			} else if j == i+1 {
+			} else if j == i + 1 {
 				dp[i][j] = s[i] == s[j]
 			} else {
 				dp[i][j] = dp[i+1][j-1] && s[i] == s[j]
 			}
-			if dp[i][j] && j-i+1 > palindromeLen {
-				palindromeLen = j - i + 1
+			if dp[i][j] && j - i + 1 > l {
+				l = j - i + 1
 				start = i
 				end = j
 			}
 		}
 	}
-	return s[start : end+1]
+	return s[start:end+1]
 }
--- lc5/ac2/2.go
+++ lc5/ac2/2.go
@@ -10,27 +10,27 @@ func longestPalindrome(s string) string {
 	start, end := 0, 0
 	for i := 0; i < l; i++ {
 		for j := 0; i-j >= 0 && i+j < l; j++ {
-			if s[i-j] != s[i + j] {
+			if s[i-j] != s[i+j] {
 				break
 			}
-			if 2 * j + 1 > palindromeLen {
-				palindromeLen = 2 * j + 1
+			if 2*j+1 > palindromeLen {
+				palindromeLen = 2*j + 1
 				start = i - j
 				end = i + j
 			}
 		}
-		if i != l - 1 && s[i] == s[i + 1] {
-			for j := 0; i - j >=0 && i + j + 1 < l; j++ {
-				if s[i - j] != s[i + j + 1] {
+		if i != l-1 && s[i] == s[i+1] {
+			for j := 0; i-j >= 0 && i+j+1 < l; j++ {
+				if s[i-j] != s[i+j+1] {
 					break
 				}
-				if 2 * j + 2 > palindromeLen {
-					palindromeLen = 2 * j + 2
+				if 2*j+2 > palindromeLen {
+					palindromeLen = 2*j + 2
 					start = i - j
 					end = i + j + 1
 				}
 			}
 		}
 	}
-	return s[start:end+1]
+	return s[start : end+1]
 }
