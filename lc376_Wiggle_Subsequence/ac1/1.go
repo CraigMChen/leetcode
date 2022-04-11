3ac9624d74b7cb9048ddc6b91b5e416f71f75e0e
CraigMChen <476479827@qq.com>
2022-04-11 21:53:49 +0800

    update lc376

--- lc376_Wiggle_Subsequence/ac1/1.go
+++ lc376_Wiggle_Subsequence/ac1/1.go
@@ -1,23 +1,22 @@
 package ac1
 
+// 动态规划
+// 设 up[i] 表示以下标 i 结尾的摆动上升序列（最后一段为上升序列的摆动序列）
+// down[i] 表示以下标 i 结尾的摆动下降序列（最后一段为下降序列的摆动序列），则
+// up[i] = up[i-1] (nums[i] <= nums[i-1])
+//       = max(up[i-1], down[i-1]+1) (nums[i] < nums[i-1])
+// down[i] = down[i-1] (nums[i] >= nums[i-1])
+//         = max(down[i-1], up[i-1]+1) (nums[i] < nums[i-1])
 func wiggleMaxLength(nums []int) int {
-	up := make([]int, len(nums))
-	down := make([]int, len(nums))
-	res := 1
-	for i := 0; i < len(nums); i++ {
-		up[i] = 1
-		down[i] = 1
-		for j := 0; j < i; j++ {
-			if nums[i] > nums[j] {
-				up[i] = max(up[i], down[j] + 1)
-			}
-			if nums[i] < nums[j] {
-				down[i] = max(down[i], up[j] + 1)
-			}
+	up, down := 1, 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
+			down = max(down, up+1)
+		} else if nums[i] > nums[i-1] {
+			up = max(up, down+1)
 		}
-		res = max(max(up[i], down[i]), res)
 	}
-	return res
+	return max(up, down)
 }
 
 func max(a, b int) int {
