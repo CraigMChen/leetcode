d5a32d29fef4778b4ef13fe7070c57a8dd69bb41
CraigMChen <476479827@qq.com>
2022-05-25 21:42:46 +0800

    update lc528

--- lc528_Random_Pick_with_Weight/ac1/1.go
+++ lc528_Random_Pick_with_Weight/ac1/1.go
@@ -1,32 +1,38 @@
 package ac1
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 type Solution struct {
 	preSum []int
 }
 
-
 func Constructor(w []int) Solution {
-	preSum := make([]int, len(w))
-	preSum[0] = w[0]
+	rand.Seed(time.Now().UnixNano())
 	for i := 1; i < len(w); i++ {
-		preSum[i] = preSum[i - 1] + w[i]
+		w[i] += w[i-1]
 	}
-	return Solution{preSum: preSum}
+	return Solution{preSum: w}
 }
 
-
-// 求出数组w的前缀和，则w中的每个元素都可以看作是数轴上的线段，在线段上随机取一个点，该点落在某个线段上的概率与线段的长度成正比
+// 二分查找
+// 把权重想象成一条线段，所有线段首位相连
+// 在连接后的整条线段上随机取一点
+// 该点落在线段上的几率即为符合权重的几率
+// 对权重求前缀和preSum
+// 在[0,preSum[n-1])中随机取一个数
+// 用二分查找找到第一个大于n的元素下标即为答案
 func (this *Solution) PickIndex() int {
-	n := rand.Intn(this.preSum[len(this.preSum) - 1]) + 1
+	n := rand.Intn(this.preSum[len(this.preSum)-1])
 	l, r := 0, len(this.preSum)
 	for l < r {
-		m := (r - l) >> 1 + l
-		if this.preSum[m] < n {
-			l = m + 1
-		} else {
+		m := (r-l)>>1 + l
+		if this.preSum[m] > n {
 			r = m
+		} else {
+			l = m + 1
 		}
 	}
 	return l
