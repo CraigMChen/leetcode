eed99122b1170a7a5bc98986add6340856304704
CraigMChen <476479827@qq.com>
2022-06-13 21:36:56 +0800

    update lc911 lc1201

--- lc1201_Ugly_Number_III/ac1/1.go
+++ lc1201_Ugly_Number_III/ac1/1.go
@@ -5,38 +5,28 @@ package ac1
 // num = x / a + x / b + x / c - x / lcm(a, b) - x / lcm(a, c) - lcm(b, c) + lcm(a, b, c)
 // 则可以用二分搜索求出满足在区间[1,x]内丑数个数大于等于n的第一个数x，即为答案
 func nthUglyNumber(n int, a int, b int, c int) int {
-	lcmAB := lcm(a, b)
-	lcmAC := lcm(a, c)
-	lcmBC := lcm(b, c)
-	lcmABC := lcm(lcmAB, c)
+	var gcd, lcm func(a, b int) int
+	gcd = func(a, b int) int {
+		if b == 0 {
+			return a
+		}
+		return gcd(b, a%b)
+	}
+	lcm = func(a, b int) int {
+		return a * b / gcd(a, b)
+	}
 
-	l, r := min(a, min(b, c)), int(2e9)
+	getK := func(m int) int {
+		return m/a + m/b + m/c - m/lcm(a, b) - m/lcm(a, c) - m/lcm(b, c) + m/lcm(lcm(a, b), c)
+	}
+	var l, r int = 1, 2e9 + 1
 	for l < r {
-		m := (r - l) >> 1 + l
-		num := m / a + m / b + m / c - m / lcmAB - m / lcmBC - m / lcmAC + m / lcmABC
-		if num < n {
-			l = m + 1
-		} else {
+		m := (r-l)>>1 + l
+		if getK(m) >= n {
 			r = m
+		} else {
+			l = m + 1
 		}
 	}
 	return l
-}
-
-func gcd(a, b int) int {
-	if b == 0 {
-		return a
-	}
-	return gcd(b, a%b)
-}
-
-func lcm(a, b int) int {
-	return a * b / gcd(a, b)
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
--- lc911_Online_Election/ac1/1.go
+++ lc911_Online_Election/ac1/1.go
@@ -1,40 +1,36 @@
 package ac1
 
 type TopVotedCandidate struct {
-	winner []int
-	times []int
+	winners []int
+	times   []int
 }
 
 // 预处理，先计算出投了第i张选票时的获胜者
 func Constructor(persons []int, times []int) TopVotedCandidate {
-	max := -1
-	p := -1
-	counter := make(map[int]int)
-	winner := make([]int, len(persons))
+	count := make([]int, len(persons))
+	max := 0
+	winner := 0
 	for i := 0; i < len(persons); i++ {
-		counter[persons[i]]++
-		if counter[persons[i]] >= max {
-			max = counter[persons[i]]
-			p = persons[i]
+		count[persons[i]]++
+		if count[persons[i]] >= max {
+			max = count[persons[i]]
+			winner = persons[i]
 		}
-		winner[i] = p
+		persons[i] = winner
 	}
-	return TopVotedCandidate{
-		winner: winner,
-		times:  times,
-	}
+	return TopVotedCandidate{winners: persons, times: times}
 }
 
 // 二分搜索，找出时刻t投到第l张选票（upper_bound），则winner[l-1]即为答案
 func (this *TopVotedCandidate) Q(t int) int {
 	l, r := 0, len(this.times)
 	for l < r {
-		m := (r - l) >> 1 + l
-		if this.times[m] <= t {
-			l = m + 1
-		} else {
+		m := (r-l)>>1 + l
+		if this.times[m] > t {
 			r = m
+		} else {
+			l = m + 1
 		}
 	}
-	return this.winner[l - 1]
+	return this.winners[l-1]
 }
