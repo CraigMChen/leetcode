e6136929c8b45b3394929bdf6482d7c1b3909d1b
CraigMChen <476479827@qq.com>
2022-07-28 21:27:39 +0800

    update lc162

--- lc162_Find_Peak_Element/ac1/1.go
+++ lc162_Find_Peak_Element/ac1/1.go
@@ -5,7 +5,7 @@ func findPeakElement(nums []int) int {
 	l, r := 0, len(nums)
 	for l < r {
 		m := (r-l)>>1 + l
-		if m != len(nums) && nums[m] < nums[m+1] { // 如果 m 不是最后有一个元素且 nuns[m] < nums[m+1]
+		if m < len(nums)-1 && nums[m] < nums[m+1] { // 如果 m 不是最后有一个元素且 nuns[m] < nums[m+1]
 			l = m + 1 // 则右侧一定存在峰值
 		} else { // 否则
 			r = m // 左侧一定存在峰值
