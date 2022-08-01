4ab8d52d02bfd5ecf65b38dbde35877d5c66fb1f
CraigMChen <476479827@qq.com>
2022-08-01 20:38:35 +0800

    update lc611

--- lc611_Valid_Triangle_Number/ac2/2.go
+++ lc611_Valid_Triangle_Number/ac2/2.go
@@ -2,6 +2,11 @@ package ac2
 
 import "sort"
 
+// 双指针
+// 首先将数组排序，三条边的下标 i, j, k 满足
+// nums[i] + nums[j] > nums[k] (i < j < k)
+// 对于一个固定的 i，当 j 增加时，k 一定要增加才能满足上面的不等式
+// 枚举 i，用双指针法求 j 和 k
 func triangleNumber(nums []int) int {
 	sort.Ints(nums)
 	res := 0
