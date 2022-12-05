92b59f9fe4424ef0317712d2d8e43ce896f4e0f3
craig <craig.chen@openpie.com>
2022-12-05 21:51:17 +0800

    update lc297

--- lc297_Serialize_and_Deserialize_Binary_Tree/ac2/2.go
+++ lc297_Serialize_and_Deserialize_Binary_Tree/ac2/2.go
@@ -13,26 +13,21 @@ func Constructor() Codec {
 	return Codec{}
 }
 
-var nilValue = 1001
+var nilValue int16 = 1001
 
 // 位运算 优化
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
 	var res []byte
 	convert := func(n int) {
-		var neg byte = 0
-		if n < 0 {
-			neg = 1 << 7
-			n *= -1
-		}
-		low := byte(n & (1<<8 - 1))
-		top := byte(n>>8) | neg
+		low := byte(int16(n) & (1<<8 - 1))
+		top := byte(int16(n) >> 8)
 		res = append(res, top, low)
 	}
 	var dfs func(node *TreeNode)
 	dfs = func(node *TreeNode) {
 		if node == nil {
-			convert(nilValue)
+			convert(int(nilValue))
 			return
 		}
 		convert(node.Val)
@@ -45,16 +40,10 @@ func (this *Codec) serialize(root *TreeNode) string {
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *TreeNode {
-	var ser []int
+	var ser []int16
 	for i := 0; i < len(data); i += 2 {
 		top, low := data[i], data[i+1]
-		neg := 1
-		if top>>7 == 1 {
-			neg = -1
-			top &= 1<<7 - 1
-		}
-		n := int(top)<<8 | int(low)
-		n *= neg
+		n := int16(top)<<8 | int16(low)
 		ser = append(ser, n)
 	}
 	var build func() *TreeNode
@@ -69,7 +58,7 @@ func (this *Codec) deserialize(data string) *TreeNode {
 			return nil
 		}
 		return &TreeNode{
-			Val:   n,
+			Val:   int(n),
 			Left:  build(),
 			Right: build(),
 		}
