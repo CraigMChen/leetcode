5744597b7c906e08cdd2134e02d11e765e878b45
Craig <476479827@qq.com>
2024-04-07 21:36:31 +0800

    add lc80

--- lc80_Remove_Duplicates_from_Sorted_Array_II/ac1/1.go
+++ lc80_Remove_Duplicates_from_Sorted_Array_II/ac1/1.go
@@ -1,1 +1,19 @@
 package ac1
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 2 {
+		return len(nums)
+	}
+	tmp1, tmp2 := nums[0], nums[1]
+	p := 2
+	for i := 2; i < len(nums); i++ {
+		if nums[i] != tmp1 || nums[i] != tmp2 {
+			tmp1, tmp2 = tmp2, nums[i]
+			nums[p] = nums[i]
+			p++
+		} else {
+			tmp1, tmp2 = tmp2, nums[i-1]
+		}
+	}
+	return p
+}
