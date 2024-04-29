0b10994acf97c73846ce23ef6c6c5cd3fc6885a1
Craig <476479827@qq.com>
2024-04-29 21:20:17 +0800

    update lc55

--- lc55_Jump_Game/ac2/2.go
+++ lc55_Jump_Game/ac2/2.go
@@ -6,17 +6,12 @@ package ac2
 // 遍历过程中若当前位置比最大可达位置大，则不能走完
 // 遍历过程中若最大可达位置大于等于nums长度，则能走完
 func canJump(nums []int) bool {
-	max := 0
-	for i := range nums {
-		if i > max {
+	dp := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp < i {
 			return false
 		}
-		if nums[i] + i > max {
-			max = nums[i] + i
-		}
-		if max >= len(nums)-1 {
-			return true
-		}
+		dp = max(dp, i+nums[i])
 	}
-	return false
+	return true
 }
