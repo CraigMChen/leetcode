eef2c50bdb541031aec6b16d55eb55a01083d4a8
Craig <476479827@qq.com>
2024-05-03 20:28:31 +0800

    update lc45

--- lc45_Jump_Game_II/ac1/1.go
+++ lc45_Jump_Game_II/ac1/1.go
@@ -1,8 +1,8 @@
 package ac1
 
 // 贪心
-// 遍历nums，维护一个当前能到达的最远距离maxPos，以及最远的下标end
-// 走到下标end时更新步数，并更新end=maxPos
+// 遍历nums，维护一个当前能到达的最远距离 maxPos，以及本次跳跃能到达的最远距离 end
+// 走到下标 end 时更新步数，并更新 end = maxPos
 func jump(nums []int) int {
 	maxPos, end, step := 0, 0, 0
 	for i := 0; i < len(nums)-1; i++ {
@@ -13,11 +13,4 @@ func jump(nums []int) int {
 		}
 	}
 	return step
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
