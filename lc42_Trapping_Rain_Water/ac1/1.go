512d03ba619ed212688144c04f29fcea7b5c6ea3
Craig <476479827@qq.com>
2024-05-10 20:54:49 +0800

    update lc42

--- lc42_Trapping_Rain_Water/ac1/1.go
+++ lc42_Trapping_Rain_Water/ac1/1.go
@@ -1,38 +1,17 @@
 package ac1
 
 // 预处理
-// 设 maxLeft[i] 表示 height[0:i+1] 的最大值
-// maxRight[i] 表示height[i:] 的最大值
-// 则位置i可以接的雨水量为 min(maxLeft[i], maxRight[i]) - height[i]
-// 将每个位置的结果累加起来即为答案
-// 其中 maxLeft 可以在遍历的过程中求得， maxRight 可以通过预处理后缀数组求得
+// 分别求出前缀最大值 preMax 和 后缀最大值 sufMax
+// 则位置 i 能够接到的雨水为 max(min(preMax[i-1], sufMax[i+1])-height[i], 0)
 func trap(height []int) int {
-	maxRight := make([]int, len(height))
-	tmp := 0
+	sufMax := make([]int, len(height)+1)
 	for i := len(height) - 1; i >= 0; i-- {
-		tmp = max(tmp, height[i])
-		maxRight[i] = tmp
+		sufMax[i] = max(sufMax[i+1], height[i])
 	}
-
-	ans := 0
-	maxLeft := 0
+	preMax, ans := 0, 0
 	for i := 0; i < len(height); i++ {
-		maxLeft = max(maxLeft, height[i])
-		ans += min(maxLeft, maxRight[i]) - height[i]
+		ans += max(min(preMax, sufMax[i+1])-height[i], 0)
+		preMax = max(preMax, height[i])
 	}
 	return ans
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
 }
