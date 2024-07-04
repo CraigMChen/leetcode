ba329c0b754f1e3d94628bd9550f3b5d1b455e7b
Craig <476479827@qq.com>
2024-07-04 20:59:22 +0800

    add lc128

---
+++ lc128_Longest_Consecutive_Sequence/ac1/1.go
@@ -0,0 +1,24 @@
+package ac1
+
+// 哈希表保存以每个数为最大值可以组成的最长连续子序列的长度
+func longestConsecutive(nums []int) int {
+	counter := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		counter[nums[i]] = 1 // 初始状态，所有数可以由本身组成长度为 1 的序列
+	}
+
+	ans := 0
+	for num, l := range counter {
+		n := num - 1
+		for counter[n] > 0 { // 不断寻找比当前数小 1 的数
+			l += counter[n] // 将长度累加
+			counter[n] = 0  // 查找过的数清空，避免重复查找
+			n--
+		}
+		counter[num] = l // 更新当前长度
+		if l > ans {
+			ans = l
+		}
+	}
+	return ans
+}
---
+++ lc128_Longest_Consecutive_Sequence/ac2/2.go
@@ -0,0 +1,26 @@
+package ac2
+
+// 哈希表保存 nums 中数字的集合
+func longestConsecutive(nums []int) int {
+	set := make(map[int]struct{}, len(nums))
+	for i := 0; i < len(nums); i++ {
+		set[nums[i]] = struct{}{}
+	}
+
+	ans := 0
+	for num := range set {
+		if _, ok := set[num-1]; ok { // 若存在比当前值更小的数，跳过
+			continue
+		}
+		l := 1
+		n := num
+		for _, ok := set[n+1]; ok; _, ok = set[n+1] { // 不断寻找比当前值大 1 的数
+			n++
+			l++
+		}
+		if l > ans {
+			ans = l
+		}
+	}
+	return ans
+}
