a87f04d965f975f66b63108f5c5bccc55aeb16c1
Craig <476479827@qq.com>
2024-08-20 21:01:25 +0800

    update lc101

--- lc101_Symmetric_Tree/ac2/2.go
+++ lc101_Symmetric_Tree/ac2/2.go
@@ -7,28 +7,27 @@ type TreeNode struct {
 }
 
 func isSymmetric(root *TreeNode) bool {
-	var que1, que2 []*TreeNode
-	que1 = append(que1, root)
-	que2 = append(que2, root)
-
-	for len(que1) != 0 && len(que2) != 0 {
-		var next1, next2 []*TreeNode
-		for i := 0; i < len(que1); i++ {
-			if que1[i] == nil && que2[i] == nil {
-				continue
-			}
-			if que1[i] != nil && que2[i] != nil {
-				if que1[i].Val != que2[i].Val {
-					return false
-				}
-				next1 = append(next1, que1[i].Left, que1[i].Right)
-				next2 = append(next2, que2[i].Right, que2[i].Left)
-			} else {
-				return false
-			}
-		}
-		que1 = next1
-		que2 = next2
+	if root == nil {
+		return true
 	}
-	return true
+	que1 := []*TreeNode{root.Left}
+	que2 := []*TreeNode{root.Right}
+	for len(que1) > 0 && len(que2) > 0 {
+		node1 := que1[len(que1)-1]
+		que1 = que1[:len(que1)-1]
+		node2 := que2[len(que2)-1]
+		que2 = que2[:len(que2)-1]
+		if node1 == nil && node2 == nil {
+			continue
+		}
+		if node1 == nil || node2 == nil {
+			return false
+		}
+		if node1.Val != node2.Val {
+			return false
+		}
+		que1 = append(que1, node1.Left, node1.Right)
+		que2 = append(que2, node2.Right, node2.Left)
+	}
+	return len(que1) == len(que2)
 }
