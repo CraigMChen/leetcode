74f03fa949b8dd3abfe02f456407a8c55c108132
Craig <476479827@qq.com>
2024-08-29 21:36:58 +0800

    update lc112

--- lc112_Path_Sum/ac2/2.go
+++ lc112_Path_Sum/ac2/2.go
@@ -16,23 +16,24 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
 	for len(nodeQueue) != 0 {
 		var (
 			nodeNext []*TreeNode
-			valNext []int
+			valNext  []int
 		)
 		for i := 0; i < len(nodeQueue); i++ {
 			node := nodeQueue[i]
 			val := valQueue[i]
 			if node.Left == nil && node.Right == nil {
-				if node.Val + val == targetSum {
+				if node.Val+val == targetSum {
 					return true
 				}
+				continue
 			}
 			if node.Left != nil {
 				nodeNext = append(nodeNext, node.Left)
-				valNext = append(valNext, node.Val + val)
+				valNext = append(valNext, node.Val+val)
 			}
 			if node.Right != nil {
 				nodeNext = append(nodeNext, node.Right)
-				valNext = append(valNext, node.Val + val)
+				valNext = append(valNext, node.Val+val)
 			}
 		}
 		nodeQueue = nodeNext
