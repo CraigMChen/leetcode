883f91710837ce97f46d5ad404b35d410f583f4a
craig <craig.chen@openpie.com>
2025-07-14 18:12:31 +0800

    update lc72

--- lc72_Edit_Distance/ac1/1.go
+++ lc72_Edit_Distance/ac1/1.go
@@ -20,30 +20,24 @@ package ac1
 // 边界条件：
 // 当 i == 0 时，dp[i][j] = j
 // 当 j == 0 时，dp[i][j] = i
+// 可以使用滚动数组优化空间
 func minDistance(word1 string, word2 string) int {
-	dp := make([][]int, len(word1)+1)
+	dp := make([]int, len(word2)+1)
+	x := 0 // 表示 dp[i-1][j-1]
 	for i := 0; i <= len(word1); i++ {
-		dp[i] = make([]int, len(word2)+1)
 		for j := 0; j <= len(word2); j++ {
-			if i == 0 && j != 0 {
-				dp[i][j] = j
-			} else if i != 0 && j == 0 {
-				dp[i][j] = i
-			} else if i != 0 && j != 0 {
-				if word1[i-1] != word2[j-1] {
-					dp[i][j] = min(min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]+1)
-				} else {
-					dp[i][j] = min(min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1])
-				}
+			tmp := dp[j]
+			if i == 0 {
+				dp[j] = j
+			} else if j == 0 {
+				dp[j] = i
+			} else if word1[i-1] == word2[j-1] {
+				dp[j] = min(dp[j]+1, dp[j-1]+1, x)
+			} else {
+				dp[j] = min(dp[j]+1, dp[j-1]+1, x+1)
 			}
+			x = tmp
 		}
 	}
-	return dp[len(word1)][len(word2)]
-}
-
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
+	return dp[len(word2)]
 }
