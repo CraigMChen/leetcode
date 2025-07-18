34dfa052ab4e5acf95f7e0f32bf92b973c20a89f
craig <craig.chen@openpie.com>
2025-07-18 17:56:39 +0800

    update lc145

--- lc199_Binary_Tree_Right_Side_View/ac2/2.go
+++ lc199_Binary_Tree_Right_Side_View/ac2/2.go
@@ -7,6 +7,9 @@ type TreeNode struct {
 }
 
 // 深搜
+// 先对右子节点深搜再对左子节点深搜，这样每层第一次遇到的节点一定是改层右视图的节点
+// 标记当前深度 level，以及答案数组
+// 若 level 大于 ans 数组的长度，说明当前 level 是第一次遍历到，当前节点加入答案数组
 func rightSideView(root *TreeNode) []int {
 	var (
 		ans []int
@@ -19,8 +22,8 @@ func rightSideView(root *TreeNode) []int {
 		if level >= len(ans) {
 			ans = append(ans, node.Val)
 		}
-		dfs(node.Right, level + 1)
-		dfs(node.Left, level + 1)
+		dfs(node.Right, level+1)
+		dfs(node.Left, level+1)
 	}
 	dfs(root, 0)
 	return ans
