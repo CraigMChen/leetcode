e60a3cdb9795c5cb11981d58b513c14054e76c0a
craig <craig.chen@openpie.com>
2025-08-11 17:53:24 +0800

    update lc76

--- lc76_Minimum_Window_Substring/ac1/1.go
+++ lc76_Minimum_Window_Substring/ac1/1.go
@@ -2,48 +2,52 @@ package ac1
 
 // 滑动窗口
 // 与 lc30/lc438 的 ac2 思想类似
-// 用 map count 统计当前子串和 t 的每个字母的数量差（只需考虑 t 中出现过的字母）
-// 用 diff 表示当前子串中出现次数大于等于 t 中出现次数的字母个数
-// 当 diff > 0 时，说明当前子串不符合要求，需要扩大，即将右边界向右移动
-// 当 diff <= 0 时，说明当前子串符合要求，将左边界向右移动
+// 用 map count 统计 t 中字母的出现次数与窗口中字母出现次数的差（只需考虑 t 中出现过的字母）
+// 区别是，用 diff 表示 t 中的出现次数大于窗口中出现次数的字母的个数
+// 即 count 中大于 0 的元素数量
+// 当 diff > 0 时，说明当前窗口表示的子串不符合要求，需要扩大，即将右边界向右移动
+// 当 diff <= 0 时，说明当前窗口表示的子串符合要求，将左边界向右移动，并更新答案
 func minWindow(s string, t string) string {
 	count := make(map[byte]int)
-	diff := 0
 	for i := 0; i < len(t); i++ {
-		_, ok := count[t[i]]
-		if !ok {
-			diff++
-		}
 		count[t[i]]++
 	}
 
-	l, r := 0, 0 // 表示结果的左闭右开区间
-	for i, j := 0, 0; j <= len(s); {
-		if diff > 0 { // 此时，当前子串中的字母不够，需要将右边界右移
-			if j == len(s) {
-				break
-			}
-			_, ok := count[s[j]]
-			if ok { // 只需考虑 t 中出现过的字母
-				if count[s[j]] == 1 {
-					diff--
-				}
-				count[s[j]]--
-			}
-			j++
-		} else { // 此时，当前子串中的字母足够，需要将左边界右移
-			if r-l == 0 || j-i <= r-l {
-				l, r = i, j
-			}
-			_, ok := count[s[i]]
-			if ok {
-				if count[s[i]] == 0 {
-					diff++
-				}
-				count[s[i]]++
-			}
-			i++
+	diff := 0
+	for _, c := range count {
+		if c > 0 {
+			diff++
 		}
 	}
-	return s[l:r]
+
+	ans := ""
+	minLen := 100001
+	l, r := 0, 0
+	for r <= len(s) {
+		if diff > 0 { // diff 大于 0，说明此时窗口表示的子串不符合要求，需要将右边界右移
+			if r == len(s) { // r 已经到最右了，但是窗口还是不符合要求
+				break
+			}
+			if _, ok := count[s[r]]; ok {
+				count[s[r]]--
+				if count[s[r]] == 0 { // count[s[r]] 从 1 变成 0，t 中出现次数大于窗口中出现次数的字母个数变少了
+					diff--
+				}
+			}
+			r++
+		} else { // diff 小于等于 0，说明此时窗口表示的子串符合要求，尝试将左边界右移来使子串尽可能短
+			if r-l < minLen {
+				ans = s[l:r]
+				minLen = r - l
+			}
+			if _, ok := count[s[l]]; ok {
+				count[s[l]]++
+				if count[s[l]] == 1 { // count[s[l]] 从 0 变成 1，t 中出现次数大于窗口个中出现次数的字母个数变多了
+					diff++
+				}
+			}
+			l++
+		}
+	}
+	return ans
 }
