d1a9fb9473e6092df8f07f827edca937dac8a796
craig <craig.chen@openpie.com>
2025-09-03 21:05:42 +0800

    add lc460

---
+++ lc460_LFU_Cache/ac1/1.go
@@ -0,0 +1,120 @@
+package ac1
+
+// 双哈希表
+// 一个哈希表的键为访问次数，值为一个双向链表，链表节点保存 key, value 和使用次数
+// 另一个哈希表的键为 key，值为双向链表的节点地址
+// 需要用一个 minCount 变量来维护当前的最小使用次数
+// 时间复杂度 O(1)
+// 空间复杂度 O(n)
+
+type node struct {
+	key, val     int
+	parent, next *node
+	count        int
+}
+
+type cache struct {
+	head, tail *node
+}
+
+func (c *cache) remove(n *node) {
+	parent := n.parent
+	next := n.next
+	parent.next = next
+	next.parent = parent
+}
+
+func (c *cache) add(n *node) {
+	first := c.head.next
+	c.head.next = n
+	n.parent = c.head
+	n.next = first
+	first.parent = n
+}
+
+func newCache() *cache {
+	head := &node{}
+	tail := &node{}
+	head.next = tail
+	tail.parent = head
+	return &cache{
+		head: head,
+		tail: tail,
+	}
+}
+
+type LFUCache struct {
+	lfu      map[int]*cache
+	keys     map[int]*node
+	capacity int
+	minCount int
+}
+
+func (c *LFUCache) add(n *node) {
+	cache := c.lfu[n.count]
+	if cache == nil {
+		cache = newCache()
+		c.lfu[n.count] = cache
+	}
+	cache.add(n)
+}
+
+func (c *LFUCache) remove(n *node) {
+	cache := c.lfu[n.count]
+	cache.remove(n)
+	if cache.head.next == cache.tail {
+		delete(c.lfu, n.count)
+	}
+}
+
+func Constructor(capacity int) LFUCache {
+	return LFUCache{
+		capacity: capacity,
+		lfu:      make(map[int]*cache),
+		keys:     make(map[int]*node, capacity),
+	}
+}
+
+func (c *LFUCache) Get(key int) int {
+	n, ok := c.keys[key]
+	if !ok {
+		return -1
+	}
+
+	c.remove(n)
+	if c.lfu[n.count] == nil && n.count == c.minCount {
+		c.minCount++
+	}
+	n.count++
+	c.add(n)
+	return n.val
+}
+
+func (c *LFUCache) Put(key int, value int) {
+	n, ok := c.keys[key]
+	if ok {
+		n.val = value
+		c.remove(n)
+		if c.lfu[n.count] == nil && n.count == c.minCount {
+			c.minCount++
+		}
+		n.count++
+		c.add(n)
+		return
+	}
+
+	if len(c.keys) >= c.capacity {
+		cache := c.lfu[c.minCount]
+		toRemove := cache.tail.parent
+		c.remove(toRemove)
+		delete(c.keys, toRemove.key)
+	}
+	n = &node{
+		key:   key,
+		val:   value,
+		count: 1,
+	}
+	c.add(n)
+	c.keys[key] = n
+	c.minCount = 1
+}
